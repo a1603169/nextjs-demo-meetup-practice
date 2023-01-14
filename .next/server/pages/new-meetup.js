502f627a7f5de76de735b88a6aca6c97b475ed70
seunghunbang <upi21x@gmail.com>
2023-01-14 04:22:17 +0200

    trying to deploy with netlify

---
+++ .next/server/pages/new-meetup.js
@@ -0,0 +1,334 @@
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = require('../ssr-module-cache.js');
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete installedModules[moduleId];
+/******/ 		}
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 4);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 4:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("hDrQ");
+
+
+/***/ }),
+
+/***/ "4Q3z":
+/***/ (function(module, exports) {
+
+module.exports = require("next/router");
+
+/***/ }),
+
+/***/ "5Oeh":
+/***/ (function(module, exports) {
+
+// Exports
+module.exports = {
+	"form": "NewMeetupForm_form__2wUf9",
+	"control": "NewMeetupForm_control__Vi75y",
+	"actions": "NewMeetupForm_actions__2d-nQ"
+};
+
+
+/***/ }),
+
+/***/ "F5FC":
+/***/ (function(module, exports) {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ }),
+
+/***/ "cDcd":
+/***/ (function(module, exports) {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ "hDrQ":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__("F5FC");
+
+// EXTERNAL MODULE: external "next/router"
+var router_ = __webpack_require__("4Q3z");
+
+// EXTERNAL MODULE: external "react"
+var external_react_ = __webpack_require__("cDcd");
+
+// EXTERNAL MODULE: ./components/ui/Card.js
+var Card = __webpack_require__("leqP");
+
+// EXTERNAL MODULE: ./components/meetups/NewMeetupForm.module.css
+var NewMeetupForm_module = __webpack_require__("5Oeh");
+var NewMeetupForm_module_default = /*#__PURE__*/__webpack_require__.n(NewMeetupForm_module);
+
+// CONCATENATED MODULE: ./components/meetups/NewMeetupForm.js
+
+
+
+
+
+
+function NewMeetupForm(props) {
+  const titleInputRef = Object(external_react_["useRef"])();
+  const imageInputRef = Object(external_react_["useRef"])();
+  const addressInputRef = Object(external_react_["useRef"])();
+  const descriptionInputRef = Object(external_react_["useRef"])();
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription
+    };
+    props.onAddMeetup(meetupData);
+  }
+
+  return /*#__PURE__*/Object(jsx_runtime_["jsx"])(Card["a" /* default */], {
+    children: /*#__PURE__*/Object(jsx_runtime_["jsxs"])("form", {
+      className: NewMeetupForm_module_default.a.form,
+      onSubmit: submitHandler,
+      children: [/*#__PURE__*/Object(jsx_runtime_["jsxs"])("div", {
+        className: NewMeetupForm_module_default.a.control,
+        children: [/*#__PURE__*/Object(jsx_runtime_["jsx"])("label", {
+          htmlFor: "title",
+          children: "Meetup Title"
+        }), /*#__PURE__*/Object(jsx_runtime_["jsx"])("input", {
+          type: "text",
+          required: true,
+          id: "title",
+          ref: titleInputRef
+        })]
+      }), /*#__PURE__*/Object(jsx_runtime_["jsxs"])("div", {
+        className: NewMeetupForm_module_default.a.control,
+        children: [/*#__PURE__*/Object(jsx_runtime_["jsx"])("label", {
+          htmlFor: "image",
+          children: "Meetup Image"
+        }), /*#__PURE__*/Object(jsx_runtime_["jsx"])("input", {
+          type: "url",
+          required: true,
+          id: "image",
+          ref: imageInputRef
+        })]
+      }), /*#__PURE__*/Object(jsx_runtime_["jsxs"])("div", {
+        className: NewMeetupForm_module_default.a.control,
+        children: [/*#__PURE__*/Object(jsx_runtime_["jsx"])("label", {
+          htmlFor: "address",
+          children: "Address"
+        }), /*#__PURE__*/Object(jsx_runtime_["jsx"])("input", {
+          type: "text",
+          required: true,
+          id: "address",
+          ref: addressInputRef
+        })]
+      }), /*#__PURE__*/Object(jsx_runtime_["jsxs"])("div", {
+        className: NewMeetupForm_module_default.a.control,
+        children: [/*#__PURE__*/Object(jsx_runtime_["jsx"])("label", {
+          htmlFor: "description",
+          children: "Description"
+        }), /*#__PURE__*/Object(jsx_runtime_["jsx"])("textarea", {
+          id: "description",
+          required: true,
+          rows: "5",
+          ref: descriptionInputRef
+        })]
+      }), /*#__PURE__*/Object(jsx_runtime_["jsx"])("div", {
+        className: NewMeetupForm_module_default.a.actions,
+        children: /*#__PURE__*/Object(jsx_runtime_["jsx"])("button", {
+          children: "Add Meetup"
+        })
+      })]
+    })
+  });
+}
+
+/* harmony default export */ var meetups_NewMeetupForm = (NewMeetupForm);
+// EXTERNAL MODULE: external "next/head"
+var head_ = __webpack_require__("xnum");
+
+// CONCATENATED MODULE: ./pages/new-meetup/index.js
+
+
+
+
+
+
+
+function NewMeetUpPage() {
+  const router = Object(router_["useRouter"])();
+
+  async function addMeetupHandler(enteredMeetupData) {
+    // console.log(enteredMeetupData);
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    const data = await response.json();
+    console.log(data);
+    router.push("/");
+  }
+
+  return /*#__PURE__*/Object(jsx_runtime_["jsxs"])(external_react_["Fragment"], {
+    children: [/*#__PURE__*/Object(jsx_runtime_["jsxs"])(head_["Head"], {
+      children: [/*#__PURE__*/Object(jsx_runtime_["jsx"])("title", {
+        children: "Add a New Meetup"
+      }), /*#__PURE__*/Object(jsx_runtime_["jsx"])("meta", {
+        name: "description",
+        content: "Add your own meetups and create opportunities with others for networking"
+      })]
+    }), /*#__PURE__*/Object(jsx_runtime_["jsx"])(meetups_NewMeetupForm, {
+      onAddMeetup: addMeetupHandler
+    })]
+  });
+}
+
+/* harmony default export */ var new_meetup = __webpack_exports__["default"] = (NewMeetUpPage);
+
+/***/ }),
+
+/***/ "leqP":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("F5FC");
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _Card_module_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__("rQAc");
+/* harmony import */ var _Card_module_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_Card_module_css__WEBPACK_IMPORTED_MODULE_1__);
+
+
+
+function Card(props) {
+  return /*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__["jsx"])("div", {
+    className: _Card_module_css__WEBPACK_IMPORTED_MODULE_1___default.a.card,
+    children: props.children
+  });
+}
+
+/* harmony default export */ __webpack_exports__["a"] = (Card);
+
+/***/ }),
+
+/***/ "rQAc":
+/***/ (function(module, exports) {
+
+// Exports
+module.exports = {
+	"card": "Card_card__3KSLO"
+};
+
+
+/***/ }),
+
+/***/ "xnum":
+/***/ (function(module, exports) {
+
+module.exports = require("next/head");
+
+/***/ })
+
+/******/ });
